3bccab46e5aba4b4772b9468100ce78ede41487a
Redstonneur1256 <redstonneur1256mc@gmail.com>
2023-04-01 10:11:43 +0200

    Update for v143

--- src/fr/redstonneur1256/lsl/mixins/JoinDialogMixin.java
+++ src/fr/redstonneur1256/lsl/mixins/JoinDialogMixin.java
@@ -1,15 +1,9 @@
 package fr.redstonneur1256.lsl.mixins;
 
 import arc.Core;
-import arc.func.Cons;
-import arc.scene.Element;
-import arc.scene.ui.Button;
 import arc.scene.ui.Dialog;
-import arc.scene.ui.layout.Cell;
 import arc.scene.ui.layout.Collapser;
-import arc.scene.ui.layout.Scl;
 import arc.scene.ui.layout.Table;
-import arc.struct.SnapshotSeq;
 import mindustry.gen.Icon;
 import mindustry.ui.Styles;
 import mindustry.ui.dialogs.JoinDialog;
@@ -17,15 +11,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 // make sure to inject after ModLib's mixins so that the add button is before the refresh button
 @Mixin(value = JoinDialog.class, priority = 500)
 public abstract class JoinDialogMixin {
-
-    private static final int PADDING = 3;
 
     @Shadow
     Dialog add;
@@ -36,12 +27,9 @@ public abstract class JoinDialogMixin {
         return Core.settings.getInt("lsl.serversPerRow", 3);
     }
 
-    private float getTargetWidth() {
-        return Core.graphics.getWidth() / Scl.scl() * 0.9f;
-    }
-
-    private float getServerWidth() {
-        return getTargetWidth() / getServersPerRow() - 2 * PADDING;
+    @Inject(method = "columns", at = @At("HEAD"), cancellable = true)
+    private void overrideColumns(CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(getServersPerRow());
     }
 
     @Inject(method = "setup",
@@ -68,52 +56,6 @@ public abstract class JoinDialogMixin {
             renaming = null;
             add.show();
         }).size(40f).right().padRight(3).tooltip("@server.add");
-    }
-
-    @Inject(method = "targetWidth", at = @At("HEAD"), cancellable = true)
-    private void targetWidth(CallbackInfoReturnable<Float> cir) {
-        cir.setReturnValue(getTargetWidth());
-    }
-
-    @Redirect(
-            method = { "setupRemote", "addCommunityHost", "buildServer" },
-            at = @At(value = "INVOKE", target = "Lmindustry/ui/dialogs/JoinDialog;targetWidth()F")
-    )
-    private float redirectBuildServerWidth(JoinDialog instance) {
-        return getServerWidth();
-    }
-
-    @Redirect(method = "setupRemote", at = @At(value = "INVOKE", target = "Larc/scene/ui/layout/Table;button(Larc/func/Cons;Larc/scene/ui/Button$ButtonStyle;Ljava/lang/Runnable;)Larc/scene/ui/layout/Cell;"))
-    private Cell<Button> redirectSetupRemoteButton(Table container, Cons<Button> cons, Button.ButtonStyle style, Runnable listener) {
-        return redirectButton(container, cons, style, listener, 0);
-    }
-
-    @Redirect(method = "addCommunityHost", at = @At(value = "INVOKE", target = "Larc/scene/ui/layout/Table;button(Larc/func/Cons;Larc/scene/ui/Button$ButtonStyle;Ljava/lang/Runnable;)Larc/scene/ui/layout/Cell;"))
-    private Cell<Button> redirectAddCommunityHostButton(Table container, Cons<Button> cons, Button.ButtonStyle style, Runnable listener) {
-        return redirectButton(container, cons, style, listener, 1);
-    }
-
-    private Cell<Button> redirectButton(Table container, Cons<Button> cons, Button.ButtonStyle style, Runnable listener, int minimumElements) {
-        SnapshotSeq<Element> children = container.getChildren();
-
-        boolean createNewRow = children.size <= minimumElements || ((Table) children.peek()).getChildren().size == getServersPerRow();
-
-        Table table = createNewRow ? container.table().width(getTargetWidth()).get() : (Table) children.peek();
-        if(createNewRow) {
-            container.row();
-        }
-
-        return table.button(cons, style, listener).growY();
-    }
-
-    @Redirect(method = "addCommunityHost", at = @At(value = "INVOKE", target = "Larc/scene/ui/layout/Cell;row()V"))
-    private void addCommunityHostRow(Cell<?> instance) {
-        instance.pad(PADDING);
-    }
-
-    @Redirect(method = "buildServer", at = @At(value = "INVOKE", target = "Larc/scene/ui/layout/Cell;growX()Larc/scene/ui/layout/Cell;", ordinal = 1))
-    private Cell<?> redirectBuildServerGrowY(Cell<?> instance) {
-        return instance.grow();
     }
 
 }
